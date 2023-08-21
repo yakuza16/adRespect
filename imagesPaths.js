1029249dde950647a47238bf874d64f46e52410f
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2023-08-21 22:17:04 +0200

    png-->webp

--- imagesPaths.js
+++ imagesPaths.js
@@ -1,13 +1,13 @@
 const imagesPath = [
-  "/garden-photos/garden1.png",
-  "/garden-photos/garden2.png",
-  "/garden-photos/garden3.png",
-  "/garden-photos/garden4.png",
-  "/slider1.png",
-  "/garden-photos/garden5.png",
-  "/garden-photos/garden6.png",
-  "/garden-photos/garden7.png",
-  "/garden-photos/garden1.png",
+  "/garden-photos/garden1.webp",
+  "/garden-photos/garden2.webp",
+  "/garden-photos/garden3.webp",
+  "/garden-photos/garden4.webp",
+  "/slider1.webp",
+  "/garden-photos/garden5.webp",
+  "/garden-photos/garden6.webp",
+  "/garden-photos/garden7.webp",
+  "/garden-photos/garden1.webp",
   "/garden-photos/garden2.png",
 ];
 
